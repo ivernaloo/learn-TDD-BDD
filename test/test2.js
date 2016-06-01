55f3b13aacca5e19e8846425264f7d89a0831cc3
aloo <aloocs@gmail.com>
2016-06-01 20:41:28 +0800

    加入了其本的测试代码

---
+++ test/test2.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+const expect = require('chai').expect;
+
+describe('my test', function(){
+    it('#1', function(){
+        expect(1).to.equal(1);
+    });
+
+    it('#2', function(done){
+        setTimeout(function(){
+            expect(2).to.equal(2);
+            done();
+        },1000)
+    });
+
+    it('#3', function(){
+        return new Promise((resolve, reject) => {
+                setTimeout(function(){
+                    expect(2).to.equal(2);
+                    resolve();
+                }, 2000)
+            });
+    })
+});
